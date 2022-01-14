8775543ce96a2d44b623e4facd6bf088c8cd8788
DF1229 <daanfaber1@gmail.com>
2022-01-15 00:23:10 +0100

    Added database models
    These models (`api/model/project.js` and `api/model/user.js`) will be used to populate the database with.
    They are subject to change, should that be necessary.

--- API/model/project.js
+++ API/model/project.js
@@ -1,2 +1,15 @@
 // This file defines the MongoDB schema of a project
 // This file gets called by app.js
+
+const mongoose = require("mongoose");
+
+const projectSchema = new mongoose.Schema({
+    title: {type: String},
+    author: {type: String},
+    body: {type: String},
+    date: {type: Date, default: Date.now},
+    hidden: {type: Boolean, default: false},
+    views: {type: Number, default: 0}
+});
+
+module.exports = mongoose.model("project", projectSchema);
--- API/model/user.js
+++ API/model/user.js
@@ -1,2 +1,13 @@
 // This file defines the MongoDB schema of a user
 // This file gets called by app.js
+
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    user: {type: String},
+    email: {type: String},
+    password: {type: String},
+    admin: {type: Boolean, default: false}
+});
+
+module.exports = mongoose.model("user", userSchema);
