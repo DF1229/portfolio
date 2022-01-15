592edf2001aecff08f728c7159b5e483fedcea0e
DF1229 <daanfaber1@gmail.com>
2022-01-15 10:51:49 +0100

    Implemented login handling
    Logins get directed to the same URL with a post request, which is then
    handled by the API.

--- app.js
+++ app.js
@@ -20,13 +20,45 @@ const Project = require("./API/model/project");
 const app = express();
 app.use(express.json());
 
-// =========<ROUTES>=========
+// ========<GET ROUTES>==========
 
-app.post("/login.rq", (req, res) => {
-    console.log(req);
-    res.status(200).send("OK");
+app.get("/login", (req, res) => {
+    res.status(200).sendFile(`${__dirname}/page/login.html`);
 });
 
-// ========</ROUTES>=========
+app.get("/register", (req, res) => {
+    res.status(200).sendFile(`${__dirname}/page/register.html`);
+});
+
+// ========</GET ROUTES>=========
+// ========<POST ROUTES>=========
+
+app.post("/login", async (req, res) => {
+    try {
+        // Store & validate user input
+        const {email, password} = req.body;
+        if (!(email && password)) {
+            res.status(400).send("Missing email or password");
+        }
+
+        // Find user and verify password.
+        const user = await User.findOne({ email });
+        if (user && (await bcrypt.compare(password, user.password))) {
+            const token = jwt.sign (
+                { user_id: user._id, email },
+                process.env.TOKEN_KEY,
+                { expiresIn: "2h" }
+            );
+
+            user.token = token;
+            res.status(200).json(user);
+        }
+
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+// ========</POST ROUTES>========
 
 module.exports = app;
