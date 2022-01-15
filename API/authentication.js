49fd52575d6d17efbc54d117a82c60a389f7f42d
DF1229 <daanfaber1@gmail.com>
2022-01-15 19:33:48 +0100

    Missing semicolo

--- API/authentication.js
+++ API/authentication.js
@@ -13,7 +13,7 @@ const verifyToken = (req, res, next) => {
 
     try {
         const decoded = jwt.verify(token, TOKEN_KEY);
-        req.user = decoded
+        req.user = decoded;
     } catch (error) {
         return res.status(401).send("Invalid token");
     }
