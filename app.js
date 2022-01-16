36e97a1e70032f5347cdc85753dee11215390fef
DF1229 <daanfaber1@gmail.com>
2022-01-16 12:24:50 +0100

    Removed login handling from app.js

--- app.js
+++ app.js
@@ -18,56 +18,6 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// ========<GET ROUTES>==========
 
-app.get("/login", (req, res) => {
-    res.status(200).sendFile(`${__dirname}/page/login.html`);
-});
-
-app.get("/register", (req, res) => {
-    res.status(200).sendFile(`${__dirname}/page/register.html`);
-});
-
-// ========</GET ROUTES>=========
-// ========<POST ROUTES>=========
-
-app.post("/login", async (req, res) => {
-    try {
-        // Store & validate user input
-        const {username, password} = req.body;
-        if (!(username && password)) {
-            res.status(400).send("Missing email or password");
-        }
-
-        // Find user and verify password.
-        const user = await User.findOne({ username });
-        if (user && (await bcrypt.compare(password, user.password))) {
-            // Generate and store token in database
-            const token = jwt.sign (
-                { user_id: user._id, username },
-                process.env.TOKEN_KEY,
-                { expiresIn: "2h" }
-            );
-
-            user.token = token;
-            res.status(200).json(user);
-        } else if (!user) {
-            res.status(400).send("No user by that name");
-        } else {
-            res.status(400).send("Invalid password");
-        }
-
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-app.post("/register", (req, res) => {
-    console.log(req);
-
-    res.status(200).send();
-});
-
-// ========</POST ROUTES>========
 
 module.exports = app;
