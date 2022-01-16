37163c2de788e4c82b1758a6fad13b60b2853e64
DF1229 <daanfaber1@gmail.com>
2022-01-16 22:19:14 +0100

    Added token verifying method

--- API/authentication.js
+++ API/authentication.js
@@ -8,13 +8,25 @@ module.exports = {
     generateToken(user, username) {
         const token = jwt.sign(
             { user_id: user._id, username },
-            process.env.TOKEN_KEY,
+            process.env.JWT_KEY,
             { expiresIn: "2h" }
         );
 
         return token;
     },
     verifyToken(res, req) {
+        const token = req.cookies[process.env.TOKEN_HEADER];
 
+        if (!token) {
+            return false;
+        }
+
+        try {
+            const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+            req.user = decoded;
+            return true;
+        } catch (error) {
+            return false;
+        }
     }
 }
