4e9a8f254e34a01a7dde06ac56c06c93250fbffc
Daan Faber <daanfaber1@gmail.com>
2022-03-03 13:24:14 +0100

    Installed and configured rate limiting

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('./API/database').connect();
 
 const cookieParser = require('cookie-parser');
 const express = require("express");
+const rateLimit = require('express-rate-limit');
 
 // Define app
 const app = express();
@@ -16,8 +17,16 @@ app.use(cookieParser());
 const apiRouter = require('./router/api');
 const publicRouter = require('./router/public');
 
+// Define rate limiters
+const apiLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 25, // Limit each IP to 100 request per window
+    standardHeaders: true,
+    legacyHeaders: false
+});
+
 // Set order of routers
-app.use('/api', logger, apiRouter);
+app.use('/api', apiLimiter, logger, apiRouter);
 app.use('/', logger, publicRouter);
 
 function logger(req, res, next) {
