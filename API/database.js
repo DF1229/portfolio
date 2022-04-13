b7c681be8f208c345bab80b6ee47163b97a2a867
Daan Faber <daanfaber1@gmail.com>
2022-04-13 12:14:16 +0200

    Updated to include database authentication

--- API/database.js
+++ API/database.js
@@ -2,10 +2,10 @@
 // This file gets called by public.js.
 
 const mongoose = require("mongoose");
-const { MONGO_URI, MONGO_DB } = process.env;
+const { MONGO_URI, MONGO_DB, MONGO_USER, MONGO_PASSWORD } = process.env;
 
 exports.connect = () => {
-    const conn_string = `mongodb://${MONGO_URI}/${MONGO_DB}`;
+    const conn_string = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_URI}/${MONGO_DB}?authSource=admin&authMechanism=SCRAM-SHA-256`;
     mongoose.connect(conn_string)
     .then(() => {
         console.log(`Connected to database on ${conn_string}`);
